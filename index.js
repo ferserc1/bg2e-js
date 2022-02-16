b74742f123d771ad2f3c7dd99878dd90da9ae002
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-02-16 11:59:56 +0100

    local server test file removed

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const server = express();
-server.use('/', express.static(__dirname));
-server.use('/tools', express.static(__dirname + '/bg2e-tools'));
-
-server.listen(3000);
