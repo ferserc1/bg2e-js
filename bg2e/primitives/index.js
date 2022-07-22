0aa00130444add8f1e87c3d71f53729fc2a1cd6a
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-07-22 14:28:33 +0200

    Cone primitive added

--- bg2e/primitives/index.js
+++ bg2e/primitives/index.js
@@ -1,7 +1,9 @@
 import cube from './cube';
 import sphere from './sphere';
 import cylinder from './cylinder';
+import cone from './cone';
 
 export const createCube = cube;
 export const createSphere = sphere;
 export const createCylinder = cylinder;
+export const createCone = cone;
