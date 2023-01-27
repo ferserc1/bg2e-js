42a9ce0dbda857fb5e7f5ced099b54fc2b3dbd37
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-01-27 12:14:02 +0100

    New example added: load and render scenes

---
+++ examples/22_scene_load_1/example.js
@@ -0,0 +1,39 @@
+import Canvas from "bg2e/app/Canvas";
+import MainLoop, { FrameUpdate } from "bg2e/app/MainLoop";
+import WebGLRenderer from "bg2e/render/webgl/Renderer";
+import Camera, { OpticalProjectionStrategy } from "bg2e/scene/Camera";
+import SceneAppController from "bg2e/render/SceneAppController";
+import OrbitCameraController from "bg2e/scene/OrbitCameraController";
+import Loader, { registerLoaderPlugin } from "bg2e/db/Loader";
+import Bg2LoaderPlugin from "bg2e/db/Bg2LoaderPlugin";
+import { registerComponents } from "bg2e/scene";
+
+class MyAppController extends SceneAppController {
+    async loadScene() {
+
+        // Register loader plugins
+        registerLoaderPlugin(new Bg2LoaderPlugin({ bg2ioPath: "dist/" }));
+        registerComponents();
+
+        // Load scene
+        const loader = new Loader();
+        const root = await loader.loadNode("../resources/test-scene/test-scene.vitscnj");
+
+        // Get main camera
+        const mainCamera = Camera.GetMain(root);
+        console.log(mainCamera);
+
+        // Add Orbit camera controller component to the camera node
+        return root;
+    }
+}
+
+window.onload = async () => {
+    const canvas = new Canvas(document.getElementById('gl-canvas'), new WebGLRenderer());
+    canvas.domElement.style.width = "100vw";
+    canvas.domElement.style.height = "100vh";
+    const appController = new MyAppController();
+    const mainLoop = new MainLoop(canvas, appController);
+    mainLoop.updateMode = FrameUpdate.AUTO;
+    await mainLoop.run();
+}
