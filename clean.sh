ea1c9527fd4fa927b579f8091671608e92bbcc96
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-01-29 11:39:27 +0100

    build and clean scripts updated

--- clean.sh
+++ clean.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
-rm -rf examples/base/dist
-rm -rf examples/base/node_modules
-rm -rf examples/math/dist
-rm -rf examples/math/node_modules
-rm -rf examples/tools/dist
-rm -rf examples/tools/node_modules
-rm -rf examples/db/dist
-rm -rf examples/db/node_modules
-rm -rf examples/process_detection/dist
-rm -rf examples/process_detection/node_modules
-rm -rf examples/scene/dist
-rm -rf examples/scene/node_modules
-rm -rf examples/app/dist
-rm -rf examples/app/node_modules
-rm -rf examples/webgl/dist
-rm -rf examples/webgl/node_modules
-rm -rf examples/render_1/dist
-rm -rf examples/render_1/node_modules
-rm -rf examples/present_texture/dist
-rm -rf examples/present_texture/node_modules
-rm -rf examples/render_to_texture/dist
-rm -rf examples/render_to_texture/node_modules
-rm -rf examples/sky_sphere/dist
-rm -rf examples/sky_sphere/node_modules
-rm -rf examples/render_to_cubemap/dist
-rm -rf examples/render_to_cubemap/node_modules
+clean_example() {
+    name=$1
+    rm -rf examples/$name/dist
+    rm -rf examples/$name/node_modules
+}
+
+clean_example 00_math
+clean_example 01_tools
+clean_example 02_load_model_basic
+clean_example 03_base
+clean_example 04_scene_load_0
+clean_example 05_app
+clean_example 06_present_texture
+clean_example 07_webgl
+clean_example 08_merge_textures
+clean_example 09_process_detection
+clean_example 10_render_state_basic
+clean_example 11_webgl_shader_utils
+clean_example 12_render_to_texture
+clean_example 13_render_to_cubemap
+clean_example 14_sky_sphere
+clean_example 15_sky_cube
+clean_example 16_env_renderer
+clean_example 17_pbr_basic
+clean_example 18_pbr_ibl
+clean_example 19_render_queue
+clean_example 20_scene_renderer
+clean_example 21_scene_app_controller
+clean_example 22_scene_load_1
---
+++ install_examples.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+build_example() {
+    name=$1
+    cd examples/$name
+    npm install
+    cd -
+}
+
+cd bg2e
+npm ci
+cd -
+
+./clean.sh
+
+
+build_example 00_math
+build_example 01_tools
+build_example 02_load_model_basic
+build_example 03_base
+build_example 04_scene_load_0
+build_example 05_app
+build_example 06_present_texture
+build_example 07_webgl
+build_example 08_merge_textures
+build_example 09_process_detection
+build_example 10_render_state_basic
+build_example 11_webgl_shader_utils
+build_example 12_render_to_texture
+build_example 13_render_to_cubemap
+build_example 14_sky_sphere
+build_example 15_sky_cube
+build_example 16_env_renderer
+build_example 17_pbr_basic
+build_example 18_pbr_ibl
+build_example 19_render_queue
+build_example 20_scene_renderer
+build_example 21_scene_app_controller
+build_example 22_scene_load_1
