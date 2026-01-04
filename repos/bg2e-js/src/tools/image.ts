8dd2b84bc7d51426f84bb84c897c3cb1779a22b8
Fernando Serrano Carpena <ferserc1@gmail.com>
2026-01-04 16:26:08 +0100

    feat(image): add image processing utilities for base64 conversion and hashing

--- repos/bg2e-js/src/tools/image.ts
+++ repos/bg2e-js/src/tools/image.ts
@@ -1,15 +1,18 @@
 import { generateMD5 } from '../tools/crypto';
 
-export const imageToBase64 = (image, format = 'image/jpeg') => {
+export const imageToBase64 = (image: HTMLImageElement, format = 'image/jpeg') => {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
     canvas.height = image.naturalHeight;
     canvas.width = image.naturalWidth;
+    if (ctx === null) {
+        throw new Error('Could not get canvas 2D context');
+    }
     ctx.drawImage(image, 0, 0);
     return canvas.toDataURL(format);
 }
 
-export const generateImageHash = (image) => {
+export const generateImageHash = (image: HTMLImageElement) => {
     const base64 = imageToBase64(image);
     return generateMD5(base64);
 }
